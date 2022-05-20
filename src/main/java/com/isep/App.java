8054bc9a0113496b3c6894fb48d62b5b200d3b86
jb <jb.moufle@gmail.com>
2022-05-20 10:14:58 +0200

    pane for game

--- src/main/java/com/isep/App.java
+++ src/main/java/com/isep/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         stage.getIcons().add(new Image("file:src/main/resources/img/Board.png"));
         stage.setTitle("RicochetRobot");
-        scene = new Scene(loadFXML("game"), 2*GameController.plateau.getQuarterBoardSize()[0]*GameController.plateau.getCellSize()+20, 2*GameController.plateau.getQuarterBoardSize()[1]*GameController.plateau.getCellSize()+20);
+        scene = new Scene(loadFXML("game"), 2*GameController.plateau.getQuarterBoardSize()[0]*GameController.plateau.getCellSize()+20+300, 2*GameController.plateau.getQuarterBoardSize()[1]*GameController.plateau.getCellSize()+20);
         stage.setScene(scene);
         //stage.setMaximized(true);
 
